0fc4e4d05ee45657c5e4129c5feb5447486117a5
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-05-18 16:11:00 +0200

    add config api

--- src/main/java/me/gaminglounge/moreitems/MoreItems.java
+++ src/main/java/me/gaminglounge/moreitems/MoreItems.java
@@ -1,11 +1,16 @@
 package me.gaminglounge.moreitems;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import me.gaminglounge.configapi.LoadConfig;
 import me.gaminglounge.moreitems.listener.DiscoverRecipe;
 
 public class MoreItems extends JavaPlugin {
@@ -17,6 +22,11 @@ public class MoreItems extends JavaPlugin {
     public void onLoad() {
         INSTACE = this;
         INSTACE.saveDefaultConfig();
+
+        Map<String, InputStream> lang = new HashMap<>();
+        lang.put("en_US.json", this.getResource("lang/en_US.json"));
+        lang.put("de_DE.json", this.getResource("lang/de_DE.json"));
+        LoadConfig.registerLanguage(this, lang);
 
         if (!CommandAPI.isLoaded())
             CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
